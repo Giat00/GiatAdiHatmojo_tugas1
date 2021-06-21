3b57796e9ebbc9ddc9eee957d4a5d0a957fbcb6f
Giat00 <81604393+Giat00@users.noreply.github.com>
2021-06-21 14:16:18 +0800

    membuat 6 objek pada Menu3

--- src/menu/Menu3.java
+++ src/menu/Menu3.java
@@ -1,5 +1,14 @@
 package menu;
 
-public class Menu3 {
 
+
+public class Menu3 {
+	public static void main(String [] args)  {
+		Menu1 m1 = new Menu1();
+		Menu1 m2 = new Menu1();
+		Menu1 m3 = new Menu1();
+		Menu1 m4 = new Menu2();
+		Menu1 m5 = new Menu1();
+		Menu1 m6 = new Menu1();
+	}
 }
