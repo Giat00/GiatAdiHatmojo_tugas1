26b99eecda11c62c643f794585cf6e7a04fe5d53
Giat00 <81604393+Giat00@users.noreply.github.com>
2021-06-21 14:17:38 +0800

    membuat get&set pada Menu1

--- src/menu/Menu1.java
+++ src/menu/Menu1.java
@@ -3,14 +3,14 @@ package menu;
 public class Menu1 {
 	
 	//deklerasi 8atribut
-	String nama;
-	String npm;
-	String kelas;
-	int nilai;
-	double uas;
-	double kehadiran;
-	double tugas;
-	double uts;
+	private String nama;
+	private String npm;
+	private String kelas;
+	private int nilai;
+	private double uas;
+	private double kehadiran;
+	private double tugas;
+	private double uts;
 	
 	//deklarasi 4method
 		//method untuk mencetak nilai
@@ -53,4 +53,56 @@ public class Menu1 {
 	    	this.tugas = tugas ;
 	    	this.uts = uts;
 	    }
+	  //membuat getter and setter untuk class menu1
+	    public String getnama() {
+	        return nama;
+	    }
+        public void setnama(String nama) {
+            this.nama = nama;
+        }
+        public String getnpm() {
+            return npm;
+        }
+        public void setnpm(String npm) {
+            this.npm = npm;
+        }    
+        public String getkelas() {
+            return kelas;
+        }
+        public void setkelas(String kelas) {
+            this.kelas = kelas;
+        }
+        public int getnilai() {
+            return nilai;
+        }
+        public void setnilai(int nilai) {
+            this.nilai = nilai;
+        }
+		public double getuas() {
+			return uas;
+		}
+		public void setuas(double uas) {
+			this.uas = uas;
+		}
+		public double getkehadiran() {
+			return kehadiran;
+		}
+		public void setkehadiran(double kehadiran) {
+			this.kehadiran = kehadiran;
+		}
+		public double gettugas() {
+			return tugas;
+		}
+		public void settugas(double tugas) {
+			this.tugas = tugas;
+		}
+		public double getuts() {
+			return uts;
+		}
+		public void setuts(double uts) {
+			this.uts = uts;
+		}
+        public double getnilaiakhir() {
+        	return nilaiakhir();
+        }
 }
