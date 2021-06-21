134cc728f202f8b9bd8ed6d9ffeb11b8aed87264
Giat00 <81604393+Giat00@users.noreply.github.com>
2021-06-21 14:19:12 +0800

    membuat get&set di Menu2

--- src/menu/Menu2.java
+++ src/menu/Menu2.java
@@ -37,4 +37,37 @@ public class Menu2 extends Menu1 {
         
         return ketlulus; 
 	}
+	
+	//membuat getter and setter untuk class menu2
+			public int nilai() {
+				return nilai;
+			}
+
+			public void nilai(int nilai) {
+				this.nilai = nilai;
+			}
+			
+			public String getAtribut2() {
+				return atribut2;
+			}
+
+			public void setAtribut2(String atribut2) {
+				this.atribut2 = atribut2;
+			}
+
+			public String getAtribut3() {
+				return atribut3;
+			}
+
+			public void setAtribut3(String atribut3) {
+				this.atribut3 = atribut3;
+			}
+	        
+	        //membuat ovveride
+			public void cetakketlulus() {		
+				System.out.println("Lulus");
+			}
+			public void grade1() {
+				System.out.println("B");
+		    }
 }
